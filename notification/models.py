5db6e000ba17c5241ea328b093cfc8fe718c71e5
John Groszko <grosjoh1@iit.edu>
2010-11-03 19:46:20 -0500

    User mailer for e-mail

--- notification/models.py
+++ notification/models.py
@@ -8,7 +8,6 @@ except ImportError:
 from django.db import models
 from django.db.models.query import QuerySet
 from django.conf import settings
-from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.template import Context
 from django.template.loader import render_to_string
@@ -25,6 +24,7 @@ from django.contrib.contenttypes import generic
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import ugettext, get_language, activate
 
+from mailer import send_mail
 
 QUEUE_ALL = getattr(settings, "NOTIFICATION_QUEUE_ALL", False)
 
