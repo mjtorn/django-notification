3d1fac50526f12c0e546f0a76c0a328638b8bcf6
Matt George <mgeorge@stratasan.com>
2011-12-12 08:53:50 -0600

    fixing filter that was never being applied

--- notification/models.py
+++ notification/models.py
@@ -105,7 +105,7 @@ class NoticeManager(models.Manager):
             lookup_kwargs = {"recipient": user}
         qs = self.filter(**lookup_kwargs)
         if not archived:
-            self.filter(archived=archived)
+            qs = self.filter(archived=archived)
         if unseen is not None:
             qs = qs.filter(unseen=unseen)
         if on_site is not None:
